f8b2f34507d62e692c51d40e85efe8023e132788
thgMatajs <thiago.pacheco@inchurch.com.br>
2024-05-29 19:33:21 -0300

    add ktor

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -42,4 +42,5 @@ dependencies {
     implementation(libs.compose.material3)
     implementation(libs.androidx.activity.compose)
     debugImplementation(libs.compose.ui.tooling)
+    implementation(libs.kotlinx.coroutines.android)
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -29,8 +29,17 @@ kotlin {
     }
     
     sourceSets {
+        androidMain.dependencies {
+            implementation(libs.ktor.client.okhttp)
+        }
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+        }
         commonMain.dependencies {
             //put your multiplatform dependencies here
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.core)
+            implementation(libs.kotlinx.coroutines.core)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
