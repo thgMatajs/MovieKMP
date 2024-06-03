b00ed2c82cb30224e4a5784b150dad25714e8750
thgMatajs <thiago.pacheco@inchurch.com.br>
2024-06-03 17:32:12 -0300

    adjusts to kotlin 2.0

--- shared/src/commonMain/kotlin/io/gentalha/code/movie/di/Koin.kt
+++ shared/src/commonMain/kotlin/io/gentalha/code/movie/di/Koin.kt
@@ -1,6 +1,6 @@
 package io.gentalha.code.movie.di
 
-import io.gentalha.code.movie.feature.di.featuresModule
+import io.gentalha.code.movie.feature.di.sharedModule
 import io.gentalha.code.movie.platformModule
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
@@ -8,7 +8,7 @@ import org.koin.dsl.KoinAppDeclaration
 fun initKoin(appDeclaration: KoinAppDeclaration = {}) =
     startKoin {
         appDeclaration()
-        modules(featuresModule, platformModule())
+        modules(sharedModule, platformModule())
     }
 
 // called by iOS etc
--- shared/src/commonMain/kotlin/io/gentalha/code/movie/feature/di/FeatureModule.kt
+++ shared/src/commonMain/kotlin/io/gentalha/code/movie/feature/di/FeatureModule.kt
@@ -3,6 +3,6 @@ package io.gentalha.code.movie.feature.di
 import io.gentalha.code.movie.feature.movie_list.data.di.movieListModule
 import org.koin.dsl.module
 
-val featuresModule = module {
+val sharedModule = module {
     movieListModule
 }
